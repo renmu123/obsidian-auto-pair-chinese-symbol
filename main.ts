5972e74c0456c737acd9796eef0550221bd97bd9
renmu <1101022351@qq.com>
2021-05-11 17:35:06 +0800

    更改实现方式

--- main.ts
+++ main.ts
@@ -32,6 +32,53 @@ interface PairObject {
 export default class AutoPairPlugin extends Plugin {
   settings: MyPluginSettings;
   static changeList: CodeMirror.EditorChange[] = [];
+  pairObject: PairObject = {
+    Comma: {
+      1: {
+        left: "《",
+        right: "》",
+        code: "Comma",
+        shiftKey: true,
+      },
+    },
+    Digit9: {
+      1: {
+        left: "（",
+        right: "）",
+        code: "Digit9",
+        shiftKey: true,
+      },
+    },
+    BracketLeft: {
+      0: {
+        left: "【",
+        right: "】",
+        code: "BracketLeft",
+        shiftKey: false,
+      },
+    },
+    Quote: {
+      0: {
+        left: "‘",
+        right: "’",
+        code: "Quote",
+        shiftKey: false,
+      },
+      1: {
+        left: "“",
+        right: "”",
+        code: "Quote",
+        shiftKey: true,
+      },
+    },
+  };
+  pairs = {
+    "【": "】",
+    "《": "》",
+    "（": "）",
+    "‘": "’",
+    "“": "”",
+  };
 
   async onload() {
     await this.loadSettings();
@@ -39,130 +86,33 @@ export default class AutoPairPlugin extends Plugin {
     this.registerCodeMirror(cm => {
       cm.on("change", this.change);
     });
-    this.registerDomEvent(document, "keydown", (event: KeyboardEvent) => {
-      const editor = this.getEditor();
-      const cursorInfo = this.getCursorInfo(editor);
-
-      // console.log("keydown", event.code, event.key, event);
-      const code = event.code;
-      const key = event.key;
-      const shiftKey = event.shiftKey;
-
-      const pairObject: PairObject = {
-        Comma: {
-          1: {
-            left: "《",
-            right: "》",
-            code: "Comma",
-            shiftKey: true,
-          },
-        },
-        Digit9: {
-          1: {
-            left: "（",
-            right: "）",
-            code: "Digit9",
-            shiftKey: true,
-          },
-        },
-        BracketLeft: {
-          0: {
-            left: "【",
-            right: "】",
-            code: "BracketLeft",
-            shiftKey: false,
-          },
-        },
-        Quote: {
-          0: {
-            left: "‘",
-            right: "’",
-            code: "Quote",
-            shiftKey: false,
-          },
-          1: {
-            left: "“",
-            right: "”",
-            code: "Quote",
-            shiftKey: true,
-          },
-        },
-      };
-
-      const allowSelect = this.settings.mySetting;
-
-      if (key == "Process") {
-        const pairs = pairObject[code];
-        if (pairs === undefined) {
-          return;
-        } else {
-          const pair = pairs[shiftKey ? "1" : "0"];
-          if (pair === undefined) {
-            return;
-          } else {
-            if (editor.somethingSelected()) {
-              if (allowSelect) {
-                const selected = editor.getSelection();
-                const to = cursorInfo;
-                const from = {
-                  line: cursorInfo.line,
-                  ch: cursorInfo.ch - selected.length,
-                };
-
-                const mdView = this.app.workspace.getActiveViewOfType(
-                  MarkdownView
-                );
-                mdView.sourceMode.cmEditor.replaceRange(
-                  `${pair.left}${selected}${pair.right}`,
-                  from,
-                  to,
-                  "*chineseSymbol"
-                );
-              } else {
-              }
-            } else {
-              console.log("aaa");
-              this.insert(editor, pair.right, cursorInfo, cursorInfo);
-              editor.setCursor(cursorInfo);
-            }
-          }
-        }
-      }
+    this.registerCodeMirror(cm => {
+      cm.on("beforeChange", this.beforeChange);
     });
   }
-
-  change(cm: CodeMirror.Editor, obj: CodeMirror.EditorChange) {
-    // console.log(obj);
-
-    if (AutoPairPlugin.changeList.length === 2) {
-      AutoPairPlugin.changeList.shift();
-    }
-    AutoPairPlugin.changeList.push(obj);
-
-    if (
-      AutoPairPlugin.changeList.length === 2 &&
-      AutoPairPlugin.changeList[0].origin === "*chineseSymbol"
-    ) {
-      const from = AutoPairPlugin.changeList[1].from;
-      const to = AutoPairPlugin.changeList[1].to;
-      // cm.replaceRange("", from, { line: to.line, ch: to.ch + 1 }, "*compose");
-      cm.undo();
+  change = (cm: CodeMirror.Editor, obj: CodeMirror.EditorChange) => {
+    if (obj.origin === "*compose") {
+      const symbol = obj.text[0];
+      const cursorInfo = cm.getCursor();
+      const pair = this.pairs[symbol];
+      if (pair === undefined) {
+        return;
+      }
+      if (obj.removed[0].length === 0) {
+        cm.replaceRange(pair, cursorInfo, cursorInfo, "*composeSymbol");
+        cm.setCursor(cursorInfo);
+      } else {
+      }
     }
 
     if (obj.origin === "+delete") {
-      const pairs = {
-        "【": "】",
-        "《": "》",
-        "（": "）",
-        "‘": "’",
-        "“": "”",
-      };
-      if (pairs.hasOwnProperty(obj.removed[0])) {
+      if (this.pairs.hasOwnProperty(obj.removed[0])) {
         const value = cm.getRange(
           { line: obj.from.line, ch: obj.from.ch },
           { line: obj.from.line, ch: obj.from.ch + 1 }
         );
-        if (value === pairs[obj.removed[0]]) {
+        // @ts-ignore
+        if (value === this.pairs[obj.removed[0]]) {
           const cur = cm.getCursor();
           cm.replaceRange(
             "",
@@ -173,9 +123,34 @@ export default class AutoPairPlugin extends Plugin {
         }
       }
     }
-  }
+  };
+  beforeChange = (cm: CodeMirror.Editor, obj: CodeMirror.EditorChange) => {
+    const allowSelect = this.settings.mySetting;
+
+    const symbol = obj.text[0];
+    const cursorInfo = cm.getCursor();
+    const pair = this.pairs[symbol];
+    if (pair === undefined) {
+      return;
+    }
+
+    if (allowSelect) {
+      if (cm.somethingSelected()) {
+        const selected = cm.getSelection();
+        const to = cursorInfo;
+        const from = {
+          line: cursorInfo.line,
+          ch: cursorInfo.ch - selected.length,
+        };
+        obj.update(from, to, [`${symbol}${selected}${pair}`]);
+      }
+    }
+  };
 
   onunload() {
+    this.app.workspace.iterateCodeMirrors(cm => {
+      cm.off("beforeChange", this.beforeChange);
+    });
     this.app.workspace.iterateCodeMirrors(cm => {
       cm.off("change", this.change);
     });
@@ -187,28 +162,6 @@ export default class AutoPairPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
-  }
-
-  insert(
-    editor: Editor,
-    replace: string,
-    from: EditorPosition,
-    to: EditorPosition
-  ) {
-    editor.replaceRange(replace, from, to);
-  }
-
-  getCursorInfo(editor: Editor) {
-    return editor.getCursor();
-  }
-
-  getEditor() {
-    const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    if (mdView) {
-      return mdView.editor;
-    } else {
-      return null;
-    }
   }
 }
 
@@ -232,7 +185,6 @@ class SampleSettingTab extends PluginSettingTab {
       .setDesc("如果是，选中文字后插入符号会自动包裹")
       .addToggle(text =>
         text.setValue(this.plugin.settings.mySetting).onChange(async value => {
-          console.log("Secret: " + value);
           this.plugin.settings.mySetting = value;
           await this.plugin.saveSettings();
         })
